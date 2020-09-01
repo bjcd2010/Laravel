c82dd132088d12b9f4bd8ddc54a7c84e8abbd4b1
Braulio <braulio_cerralta@hotmail.com>
2020-09-01 18:30:17 -0500

    Add Model pyme

---
+++ app/Http/Controllers/PymeController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Pyme;
+use Illuminate\Http\Request;
+
+class PymeController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Pyme  $pyme
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Pyme $pyme)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Pyme  $pyme
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Pyme $pyme)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Pyme  $pyme
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Pyme $pyme)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Pyme  $pyme
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Pyme $pyme)
+    {
+        //
+    }
+}
