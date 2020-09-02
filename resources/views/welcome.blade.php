a509eacb0859435f635cc5b1f8b16630a3348c07
Braulio <braulio_cerralta@hotmail.com>
2020-09-02 15:43:32 -0500

    Auth

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -81,7 +81,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Bienvenido Laravel UI
+                    Bienvenido Laravel UI + auth
                 </div>
 
                 
